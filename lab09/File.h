ef0251132d207b02bdd4ffefb67f84175ac2d5fd
KisBenjamin <kis.beniamin.tibor@student.ms.sapientia.ro>
2025-11-12 11:40:15 +0200

    Lab9 work and some lab5

---
+++ lab09/File.h
@@ -0,0 +1,8 @@
+//
+// Created by beni on 11/12/2025.
+//
+
+#ifndef FILE_H
+#define FILE_H
+
+#endif //FILE_H
---
+++ lab09/FileSystemItem.h
@@ -0,0 +1,8 @@
+//
+// Created by beni on 11/12/2025.
+//
+
+#ifndef FILESYSTEMITEM_H
+#define FILESYSTEMITEM_H
+
+#endif //FILESYSTEMITEM_H
